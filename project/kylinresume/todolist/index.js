1b521bea3e2fdbeeec5d25eb015c62c15bb30f9a
冉光宇 <rgy@ranguangyudeMacBook-Pro.local>
2017-01-19 00:44:28 +0800

    todolist finished

--- project/kylinresume/todolist/index.js
+++ project/kylinresume/todolist/index.js
@@ -6,10 +6,11 @@ var ongoingRef = ref.child('ongoing'),
 var num_ongoing = 0;
 wu.each(ongoingRef, function(item, id) {
   num_ongoing++;
+  item = $.safeStr(item);
   $('<li>' +
       '<input type="checkbox" class="J-checkbox" data-item=' + item + ' data-id=' + id + '>' +
-       item +
-      '<span class="glyphicon glyphicon-remove icon-del J-del-btn" id=' + id + '></span>' +
+      '<div class="item-content" title=' + item + '>' + item + '</div>' +
+      '<span class="glyphicon glyphicon-remove icon-del J-del-btn" data-item=' + item + ' id=' + id + '></span>' +
     '</li>')
   .appendTo('#J-ongoing-ul');
   $('.J-num-ongoing').html(num_ongoing);
@@ -18,10 +19,10 @@ wu.each(ongoingRef, function(item, id) {
 var num_finished = 0;
 wu.each(finishedRef, function(item, id) {
   num_finished++;
+  item = $.safeStr(item);
   $('<li>' +
-      '<input type="checkbox" class="J-checkbox" data-item=' + item + ' data-id=' + id + '>' +
-       item +
-      '<span class="glyphicon glyphicon-remove icon-del J-del-btn" id=' + id + '></span>' +
+      '<input type="checkbox" checked="checked" disabled="disabled">'+
+      '<div class="item-content" title=' + item + '>' + item + '</div>' +
     '</li>')
   .appendTo('#J-finished-ul');
   $('.J-num-finished').html(num_finished);
@@ -30,10 +31,10 @@ wu.each(finishedRef, function(item, id) {
 var num_giveup = 0;
 wu.each(giveupRef, function(item, id) {
   num_giveup++;
+  item = $.safeStr(item);
   $('<li>' +
-      '<input type="checkbox" class="J-checkbox" data-item=' + item + ' data-id=' + id + '>' +
-       item +
-      '<span class="glyphicon glyphicon-remove icon-del J-del-btn" id=' + id + '></span>' +
+      '<input type="checkbox" checked="checked" disabled="disabled">' +
+      '<div class="item-content" title=' + item + '>' + item + '</div>' +
     '</li>')
   .appendTo('#J-giveup-ul');
   $('.J-num-giveup').html(num_giveup);
@@ -50,8 +51,10 @@ $('input[name=entry]').on('keypress', function(e) {
 });
 
 $('#J-ongoing-ul').on('click', '.J-del-btn', function() {
-  var id = $(this).attr('id');
+  var id = $(this).attr('id'),
+      item = $(this).data('item');
   wu.remove(ongoingRef, id);
+  wu.add(giveupRef, item);
 });
 
 $('#J-ongoing-ul').on('click', '.J-checkbox', function() {
